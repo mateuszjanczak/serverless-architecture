b92f535312f6895a1fe66388ba149e335babcf87
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2022-06-12 14:04:16 +0200

    Add permissions to logging in Cloudwatch from Lambda

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -18,6 +18,31 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.lambda_policy_document.json
 }
 
+data "aws_iam_policy_document" "lambda_logging_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging_policy" {
+  policy = data.aws_iam_policy_document.lambda_logging_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logging_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging_policy.arn
+}
+
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = var.src
